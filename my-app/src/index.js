eff6775de5950dd8b8b4596e1dcbf9cbe4b1fc42
qinjialei <jialei.qin@thoughtworks.com>
2020-02-19 17:55:54 +0800

    1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Todo from "./todo";
 import { Todo } from "./components/todo.js";
 import { Counter } from "./components/counter.js";
 
-
-
-
 function App() {
-  // const [items, setItems] = useState([1, 2, 3]);
-  // const onDelete = index => {
-  //   setItems(items.filter((item, index2) => index !== index2))
-  // }
-
   return (
     <div>
       <Todo></Todo>
