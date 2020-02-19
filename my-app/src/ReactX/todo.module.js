b33885007f22d5e76e8a22cba448212916d84f9d
qinjialei <jialei.qin@thoughtworks.com>
2020-02-19 18:40:48 +0800

    use reatx finish todo list

--- my-app/src/ReactX/todo.module.js
+++ my-app/src/ReactX/todo.module.js
@@ -1,12 +1,16 @@
 const todo = {
   name: 'todo',
   state: {
-    todoList: []
+    list: [1],
+    inputValue: 'reactX'
   },
   reducer: {//将 reducer 内函数的 this 绑定为 state 对象
     addTodo(payload) {
-      this.todoList.push(payload)
-    }
+      this.list.push(payload)
+    },
+    changeInput(payload) {
+      this.inputValue = payload
+    },
   },
   effect: {// 将 effect 内函数的this 绑定为 reducer 对象
     asyncAddTodo(payload) {
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Todo } from "./components/todo.js";
+// import { Todo } from "./components/todo.js";
 import { Counter } from "./components/counter.js";
+
+import { Todo } from "./components/todo.reactx.js";
 
 function App() {
   return (
