a1cb07f4ac5a98164242e570d95b577a73d98509
qinjialei <jialei.qin@thoughtworks.com>
2020-02-19 18:28:21 +0800

    使用 reactx 管理 counter 组件状态

--- my-app/src/ReactX/counter.module.js
+++ my-app/src/ReactX/counter.module.js
@@ -1,12 +1,14 @@
 const counter = {
   name: 'counter',
   state: {
-    count: 0
+    count: 10
   },
   reducer: {//将 reducer 内函数的 this 绑定为 state 对象
     add(payload) {
-      this.count += payload
-      console.log("TCL: add -> this.count", this.count)
+      this.count += 1
+    },
+    add(payload) {
+      this.count -= 1
     }
   },
   effect: {// 将 effect 内函数的this 绑定为 reducer 对象
