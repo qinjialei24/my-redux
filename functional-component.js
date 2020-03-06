2ad076be376e6168a397ebbb4b59fa49811437f6
qinjialei <jialei.qin@thoughtworks.com>
2020-03-06 13:15:14 +0800

    Update

--- functional-component.js
+++
@@ -1,9 +0,0 @@
-export const Component1 = props => {
-  console.log("Component1: props", props)
-  return { tag: 'div', attrs: {}, children: [props] }
-}
-
-export const Component2 = props => {
-  console.log("Component2: props", props)
-  return { tag: 'div', attrs: {}, children: [props] }
-}
