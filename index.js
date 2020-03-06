4f49f562554e2be49e869c4572e935612de9a27b
qinjialei <jialei.qin@thoughtworks.com>
2020-03-06 12:28:41 +0800

    Update

--- index.js
+++ index.js
@@ -62,6 +62,11 @@ function adminReducer(state, action) {
 
 
 function createStore(reducer, initialState, enhancer) {
+  if (enhancer) {
+    const enhancerStore = enhancer(createStore);
+    return enhancerStore(reducer, initialState);
+  }
+
   let state = initialState
   let listeners = []
 
@@ -105,11 +110,81 @@ const allReducer = combineReducers({
   admin: adminReducer,
 })
 
-const store = createStore(allReducer, rootState)
+// const store = createStore(allReducer, rootState)
+
+const logStateMiddleware = store => dispatch => action => {
+  console.log('last state:', store.getState());
+  dispatch(action)
+  // console.log('current state:', store.getState());
+}
+
+const logTimeMiddleware = store => dispatch => action => {
+  console.log(new Date().getTime());
+  dispatch(action)
+}
+
+// store.dispatch = logTimeMiddleware(logStateMiddleware(store.dispatch))
+
+
+
+function applyMiddleware(...middlewares) {
+  return createStore => { // return rewrite createStore  func
+
+    return (reducer, initialState) => {
+      const store = createStore(reducer, initialState)
+      const chain = middlewares.map(middleware => middleware(store)); // 每一项就是一个重写 dispatch 的函数，执行后返回重写后的dispatch
+      let dispatch = store.dispatch
+
+      chain.reverse().forEach(middleware => {
+        dispatch = middleware(dispatch)
+      })
+      store.dispatch = dispatch
+      return store
+    }
+  }
+}
+
+// applyMiddleware(logStateMiddleware, logTimeMiddleware)
+
+// [a, b]
+// a(b())
+
+// /*接收旧的 createStore，返回新的 createStore*/
+// const newCreateStore = applyMiddleware(logStateMiddleware, logTimeMiddleware)(createStore);
+
+// /*返回了一个 dispatch 被重写过的 store*/
+// const store = newCreateStore(allReducer, rootState);
+
+
+const store = createStore(allReducer, rootState, applyMiddleware(logStateMiddleware, logTimeMiddleware));
+
+
+//重写 dispatch
+// const _dispatch = store.dispatch
+
+// store.dispatch = (action) => {
+//   //状态改变前
+//   console.log('last state:', store.getState());
+
+//   _dispatch(action)//改变状态
+//   //状态改变后
+//   console.log('current state:', store.getState());
+// }
+
+// const _dispatch2 = store.dispatch
+
+
+// store.dispatch = (action) => {
+//   //状态改变前
+//   console.log(new Date().getTime());
+//   _dispatch2(action)//改变状态
+//   //状态改变后
+// }
+
+
 
 store.subscribe(() => {
   const state = store.getState()
-  console.log("state", state)
 })
 
 store.dispatch({
@@ -119,3 +194,16 @@ store.dispatch({
 store.dispatch({
   type: 'changeAdminName'
 })
+
+
+
+
+
+
+
+
+// export {
+//   createStore,
+//   combineReducers,
+
+// }
